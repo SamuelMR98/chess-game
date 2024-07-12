b3d08da305d3f218fc5cbb0aa811d154e0bd277d
Samuel Mera Ricardo <samuel.mera.ricardo@gmail.com>
2024-07-11 20:37:17 -0600

    added setBoard and getBoard to ChessGame

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -133,7 +133,7 @@ public class ChessGame {
      * @param board the new board to use
      */
     public void setBoard(ChessBoard board) {
-        throw new RuntimeException("Not implemented");
+        this.board = board;
     }
 
     /**
@@ -142,6 +142,6 @@ public class ChessGame {
      * @return the chessboard
      */
     public ChessBoard getBoard() {
-        return new ChessBoard();
+        return board;
     }
 }
