1c189f1cb3d063e3a07f76a192d2a72c5887a280
Samuel Mera Ricardo <samuel.mera.ricardo@gmail.com>
2024-08-11 12:11:21 -0600

    (phase3) added AuthService

---
+++ server/src/main/java/service/AuthService.java
@@ -0,0 +1,65 @@
+package service;
+
+import dataaccess.DataAccess;
+import dataaccess.DataAccessException;
+import model.AuthData;
+import model.UserData;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import util.CodedException;
+
+public class AuthService {
+    private final DataAccess dataAccess;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    public AuthService(DataAccess dataAccess) {
+        this.dataAccess = dataAccess;
+        this.passwordEncoder = new BCryptPasswordEncoder();
+    }
+
+    /**
+     * Create a new session for a user
+     * @param user the user to create a session for
+     * @return the auth data for the new session
+     * @throws CodedException
+     */
+    public AuthData createSession(UserData user) throws CodedException {
+        try {
+            UserData existingUser = dataAccess.readUser(user.username());
+            if (existingUser != null) {
+                if (passwordEncoder.matches(user.password(), existingUser.password())) {
+                    return dataAccess.writeAuth(user.username());
+                }
+            }
+            throw new CodedException(401, "Invalid username or password");
+        } catch (DataAccessException e) {
+            throw new CodedException(500, "Internal server error");
+        }
+    }
+
+    /**
+     * Delete a session by token
+     * @param token the token of the session to delete
+     * @throws CodedException
+     */
+    public void deleteSession(String token) throws CodedException {
+        try {
+            dataAccess.deleteAuth(token);
+        } catch (DataAccessException e) {
+            throw new CodedException(500, "Internal server error");
+        }
+    }
+
+    /**
+     * Get the auth data for a session
+     * @param token the token of the session
+     * @return the auth data for the session
+     * @throws CodedException
+     */
+    public AuthData getAuth(String token) throws CodedException {
+        try {
+            return dataAccess.readAuth(token);
+        } catch (DataAccessException e) {
+            throw new CodedException(500, "Internal server error");
+        }
+    }
+}
