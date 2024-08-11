734f70098971f4a8bb090428f3a5411ac57be5a2
Samuel Mera Ricardo <samuel.mera.ricardo@gmail.com>
2024-08-11 11:49:52 -0600

    (phase3) created server servuce and added AdminService

---
+++ server/src/main/java/service/AdminService.java
@@ -0,0 +1,29 @@
+package service;
+
+import dataaccess.DataAccess;
+import dataaccess.DataAccessException;
+import util.CodedException;
+
+/**
+ * AdminService provides endpoints for the app admin
+ */
+public class AdminService {
+    private final DataAccess dataAccess;
+
+    public AdminService(DataAccess dataAccess) {
+        this.dataAccess = dataAccess;
+    }
+
+    /**
+     * Clears all data from the database (users, games, etc)
+     *
+     * @throws CodedException if there is an error clearing the data
+     */
+    public void clear() throws CodedException {
+        try {
+            dataAccess.clear();
+        } catch (DataAccessException e) {
+            throw new CodedException(500, "Server error");
+        }
+    }
+}
