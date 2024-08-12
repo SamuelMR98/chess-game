1dfdb84aebcf77e64f40fe31bb2fc4e83993154d
Samuel Mera Ricardo <samuel.mera.ricardo@gmail.com>
2024-08-12 10:58:23 -0600

    (phase6) Finished ServerFacade

--- client/src/main/java/ui/ServerFacade.java
+++ client/src/main/java/ui/ServerFacade.java
@@ -1,7 +1,10 @@
 package ui;
 
+import chess.ChessGame;
 import com.google.gson.Gson;
 import model.AuthData;
+import model.GameData;
+import server.JoinRequest;
 import util.ResponseException;
 
 import java.io.InputStream;
@@ -34,6 +37,51 @@ public class ServerFacade {
             "email", email
         );
         return this.makeRequest("POST", "/user", req, null, AuthData.class);
+    }
+
+    public AuthData login(String username, String password) throws ResponseException {
+        var req = Map.of(
+            "username", username,
+            "password", password
+        );
+        return this.makeRequest("POST", "/session", req, null, AuthData.class);
+    }
+
+    public void logout(String token) throws ResponseException {
+        this.makeRequest("DELETE", "/session", null, token, null);
+    }
+
+    public void clear() throws ResponseException {
+        this.makeRequest("DELETE", "/db", null, null, Map.class);
+    }
+
+    public GameData createGame(String token, String gameName) throws ResponseException {
+        var req = Map.of(
+            "gameName", gameName
+        );
+        return this.makeRequest("POST", "/game", req, token, GameData.class);
+    }
+
+    public GameData joinGame(String token, int gameId, ChessGame.TeamColor color) throws ResponseException {
+        var req = new JoinRequest(color, gameId);
+        this.makeRequest("PUT", "/game", req, token, GameData.class);
+        return  getGame(token, gameId);
+    }
+
+    public GameData[] listGames(String token) throws ResponseException {
+        record Response(GameData[] games) {}
+        var res = this.makeRequest("GET", "/game", null, token, Response.class);
+        return (res != null ? res.games : new GameData[0]);
+    }
+
+    private GameData getGame(String token, int gameId) throws ResponseException {
+        var games = listGames(token);
+        for (var game : games) {
+            if (game.gameID() == gameId) {
+                return game;
+            }
+        }
+        throw new ResponseException(404, "Game not found");
     }
 
     // Helper method to make a request to the server
