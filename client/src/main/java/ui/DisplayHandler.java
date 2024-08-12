5d0e6dcdde24aca314c5e0a62ca486c75ab1f712
Samuel Mera Ricardo <samuel.mera.ricardo@gmail.com>
2024-08-12 09:27:24 -0600

    (phase5) added ui package to client and created DisplayHandler interface

---
+++ client/src/main/java/ui/DisplayHandler.java
@@ -0,0 +1,9 @@
+package ui;
+
+import model.GameData;
+
+public interface DisplayHandler {
+    void updateBoard(GameData game);
+    void message(String message);
+    void error(String error);
+}
--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,8 +1,11 @@
 import chess.*;
+import server.*;
 
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
+        var port = new Server().run(8080);
+        System.out.println("♕ 240 Chess Server running on port: " + port);
     }
 }
