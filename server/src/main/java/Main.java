b38777ea100ab24e7023d9f29b51c303a92debe7
Samuel Mera Ricardo <samuel.mera.ricardo@gmail.com>
2024-08-12 16:01:19 -0600

    modify the Main.java on server

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -3,8 +3,9 @@ import server.*;
 
 public class Main {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Server: " + piece);
+        //var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+        //System.out.println("♕ 240 Chess Server: " + piece);
+        System.out.println("♕ 240 Chess Server");
         var port = new Server().run(8080);
         System.out.println("♕ 240 Chess Server running on port: " + port);
     }
